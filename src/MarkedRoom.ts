11ef8316ce3cb5734d23f77a20355fcc3738961b
Archangel <jesse@archangel.wtf>
2024-08-05 03:46:13 +0200

    Update logging

--- src/MarkedRoom.ts
+++ src/MarkedRoom.ts
@@ -23,7 +23,7 @@ export class MarkedRoom
             //Dorms 314 Marked Room
             if ((spawnpoint.template.Position.x > 180) && (spawnpoint.template.Position.x < 185) && (spawnpoint.template.Position.z > 180) && (spawnpoint.template.Position.z < 185) && (spawnpoint.template.Position.y > 6) && (spawnpoint.template.Position.y < 7))
             {
-                this.logger.logDebug(`Marked room Customs: ${spawnpoint.template.Id}`);
+                this.logger.logDebug(`Marked room (Customs) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.markedRoomConfig.multiplier.customs;
                 this.markedAddExtraItems(spawnpoint);
                 this.markedItemGroups(spawnpoint);
@@ -34,7 +34,7 @@ export class MarkedRoom
         {
             if ((spawnpoint.template.Position.x > -125) && (spawnpoint.template.Position.x < -120) && (spawnpoint.template.Position.z > 25) && (spawnpoint.template.Position.z < 30) && (spawnpoint.template.Position.y > -15) && (spawnpoint.template.Position.y < -14))
             {
-                this.logger.logDebug(`Marked room Reserve ${spawnpoint.template.Id}`);
+                this.logger.logDebug(`Marked room (Reserve) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.markedRoomConfig.multiplier.reserve;
                 this.markedAddExtraItems(spawnpoint);
                 this.markedItemGroups(spawnpoint);
@@ -42,14 +42,14 @@ export class MarkedRoom
             }
             else if ((spawnpoint.template.Position.x > -155) && (spawnpoint.template.Position.x < -150) && (spawnpoint.template.Position.z > 70) && (spawnpoint.template.Position.z < 75) && (spawnpoint.template.Position.y > -9) && (spawnpoint.template.Position.y < -8))
             {
-                this.logger.logDebug(`Marked room Reserve ${spawnpoint.template.Id}`);
+                this.logger.logDebug(`Marked room (Reserve) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.markedRoomConfig.multiplier.reserve;
                 this.markedAddExtraItems(spawnpoint);
                 this.markedItemGroups(spawnpoint);
             }
             else if ((spawnpoint.template.Position.x > 190) && (spawnpoint.template.Position.x < 195) && (spawnpoint.template.Position.z > -230) && (spawnpoint.template.Position.z < -225) && (spawnpoint.template.Position.y > -6) && (spawnpoint.template.Position.y < -5))
             {
-                this.logger.logDebug(`Marked room Reserve ${spawnpoint.template.Id}`);
+                this.logger.logDebug(`Marked room (Reserve) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.markedRoomConfig.multiplier.reserve;
                 this.markedAddExtraItems(spawnpoint);
                 this.markedItemGroups(spawnpoint);
@@ -61,7 +61,7 @@ export class MarkedRoom
             //Abandoned Factory Marked Room
             if ((spawnpoint.template.Position.x > -133) && (spawnpoint.template.Position.x < -129) && (spawnpoint.template.Position.z > 265) && (spawnpoint.template.Position.z < 275) && (spawnpoint.template.Position.y > 8.5) && (spawnpoint.template.Position.y < 11))
             {
-                this.logger.logDebug(`Marked room Streets ${spawnpoint.template.Id}`);
+                this.logger.logDebug(`Marked room (Streets) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.markedRoomConfig.multiplier.streets;
                 this.markedAddExtraItems(spawnpoint);
                 this.markedItemGroups(spawnpoint);
@@ -69,7 +69,7 @@ export class MarkedRoom
             //Chek 13 Marked Room
             else if ((spawnpoint.template.Position.x > 186) && (spawnpoint.template.Position.x < 191) && (spawnpoint.template.Position.z > 224) && (spawnpoint.template.Position.z < 229) && (spawnpoint.template.Position.y > -0.5) && (spawnpoint.template.Position.y < 1.5))
             {
-                this.logger.logDebug(`Marked room Streets ${spawnpoint.template.Id}`);
+                this.logger.logDebug(`Marked room (Streets) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.markedRoomConfig.multiplier.streets;
                 this.markedAddExtraItems(spawnpoint);
                 this.markedItemGroups(spawnpoint);
@@ -118,7 +118,7 @@ export class MarkedRoom
                         if (dist.composedKey.key == item._id)
                         {
                             dist.relativeProbability *= this.markedRoomConfig.itemGroups[group];
-                            this.logger.logDebug(`Changed ${item._tpl} to ${dist.relativeProbability}`);
+                            this.logger.logDebug(`markedItemGroups: Changed ${item._tpl} to ${dist.relativeProbability}`);
                         }
                     }
                 }
