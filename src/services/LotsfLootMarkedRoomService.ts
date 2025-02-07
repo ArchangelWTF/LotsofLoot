11c67dae2f0e0558c2bbc35270914bb11ac5fcae
Archangel <jesse@archangel.wtf>
2025-02-07 22:52:53 +0100

    Add method for checking if spawnpoint is a marked room

--- src/services/LotsfLootMarkedRoomService.ts
+++ src/services/LotsfLootMarkedRoomService.ts
@@ -27,8 +27,7 @@ export class LotsofLootMarkedRoomService {
         const spawnPointsLighthouse = this.databaseService.getTables().locations.lighthouse.looseLoot.spawnpoints;
 
         for (const spawnpoint of spawnPointsCustoms) {
-            //Dorms 314 Marked Room
-            if (spawnpoint.template.Position.x > 180 && spawnpoint.template.Position.x < 185 && spawnpoint.template.Position.z > 180 && spawnpoint.template.Position.z < 185 && spawnpoint.template.Position.y > 6 && spawnpoint.template.Position.y < 7) {
+            if (this.isMarkedRoomSpawnpoint(spawnpoint, "bigmap")) {
                 this.logger.debug(`Marked room (Customs) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.config.getConfig().markedRoom.multiplier.customs;
                 await this.markedAddExtraItemsAsync(spawnpoint);
@@ -37,17 +36,7 @@ export class LotsofLootMarkedRoomService {
         }
 
         for (const spawnpoint of spawnPointsReserve) {
-            if (spawnpoint.template.Position.x > -125 && spawnpoint.template.Position.x < -120 && spawnpoint.template.Position.z > 25 && spawnpoint.template.Position.z < 30 && spawnpoint.template.Position.y > -15 && spawnpoint.template.Position.y < -14) {
-                this.logger.debug(`Marked room (Reserve) ${spawnpoint.template.Id}`);
-                spawnpoint.probability *= this.config.getConfig().markedRoom.multiplier.reserve;
-                await this.markedAddExtraItemsAsync(spawnpoint);
-                await this.adjustMarkedItemGroupsAsync(spawnpoint);
-            } else if (spawnpoint.template.Position.x > -155 && spawnpoint.template.Position.x < -150 && spawnpoint.template.Position.z > 70 && spawnpoint.template.Position.z < 75 && spawnpoint.template.Position.y > -9 && spawnpoint.template.Position.y < -8) {
-                this.logger.debug(`Marked room (Reserve) ${spawnpoint.template.Id}`);
-                spawnpoint.probability *= this.config.getConfig().markedRoom.multiplier.reserve;
-                await this.markedAddExtraItemsAsync(spawnpoint);
-                await this.adjustMarkedItemGroupsAsync(spawnpoint);
-            } else if (spawnpoint.template.Position.x > 190 && spawnpoint.template.Position.x < 195 && spawnpoint.template.Position.z > -230 && spawnpoint.template.Position.z < -225 && spawnpoint.template.Position.y > -6 && spawnpoint.template.Position.y < -5) {
+            if (this.isMarkedRoomSpawnpoint(spawnpoint, "rezervbase")) {
                 this.logger.debug(`Marked room (Reserve) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.config.getConfig().markedRoom.multiplier.reserve;
                 await this.markedAddExtraItemsAsync(spawnpoint);
@@ -56,15 +45,7 @@ export class LotsofLootMarkedRoomService {
         }
 
         for (const spawnpoint of spawnPointsStreets) {
-            //Abandoned Factory Marked Room
-            if (spawnpoint.template.Position.x > -133 && spawnpoint.template.Position.x < -129 && spawnpoint.template.Position.z > 265 && spawnpoint.template.Position.z < 275 && spawnpoint.template.Position.y > 8.5 && spawnpoint.template.Position.y < 11) {
-                this.logger.debug(`Marked room (Streets) ${spawnpoint.template.Id}`);
-                spawnpoint.probability *= this.config.getConfig().markedRoom.multiplier.streets;
-                await this.markedAddExtraItemsAsync(spawnpoint);
-                await this.adjustMarkedItemGroupsAsync(spawnpoint);
-            }
-            //Chek 13 Marked Room
-            else if (spawnpoint.template.Position.x > 186 && spawnpoint.template.Position.x < 191 && spawnpoint.template.Position.z > 224 && spawnpoint.template.Position.z < 229 && spawnpoint.template.Position.y > -0.5 && spawnpoint.template.Position.y < 1.5) {
+            if (this.isMarkedRoomSpawnpoint(spawnpoint, "tarkovstreets")) {
                 this.logger.debug(`Marked room (Streets) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.config.getConfig().markedRoom.multiplier.streets;
                 await this.markedAddExtraItemsAsync(spawnpoint);
@@ -73,14 +54,52 @@ export class LotsofLootMarkedRoomService {
         }
 
         for (const spawnpoint of spawnPointsLighthouse) {
-            //Lightkeeper marked room
-            if (spawnpoint.template.Position.x > 319 && spawnpoint.template.Position.x < 330 && spawnpoint.template.Position.z > 482 && spawnpoint.template.Position.z < 489 && spawnpoint.template.Position.y > 5 && spawnpoint.template.Position.y < 6.5) {
+            if (this.isMarkedRoomSpawnpoint(spawnpoint, "lighthouse")) {
                 this.logger.debug(`Marked room (Lighthouse) ${spawnpoint.template.Id}`);
                 spawnpoint.probability *= this.config.getConfig().markedRoom.multiplier.lighthouse;
                 await this.markedAddExtraItemsAsync(spawnpoint);
                 await this.adjustMarkedItemGroupsAsync(spawnpoint);
             }
         }
+    }
+
+    public isMarkedRoomSpawnpoint(spawnpoint: ISpawnpoint, locationId: string): boolean {
+        if (locationId === "bigmap") {
+            //Dorms 314 Marked Room
+            if (spawnpoint.template.Position.x > 180 && spawnpoint.template.Position.x < 185 && spawnpoint.template.Position.z > 180 && spawnpoint.template.Position.z < 185 && spawnpoint.template.Position.y > 6 && spawnpoint.template.Position.y < 7) {
+                return true;
+            }
+        }
+
+        if (locationId === "rezervbase") {
+            if (spawnpoint.template.Position.x > -125 && spawnpoint.template.Position.x < -120 && spawnpoint.template.Position.z > 25 && spawnpoint.template.Position.z < 30 && spawnpoint.template.Position.y > -15 && spawnpoint.template.Position.y < -14) {
+                return true;
+            } else if (spawnpoint.template.Position.x > -155 && spawnpoint.template.Position.x < -150 && spawnpoint.template.Position.z > 70 && spawnpoint.template.Position.z < 75 && spawnpoint.template.Position.y > -9 && spawnpoint.template.Position.y < -8) {
+                return true;
+            } else if (spawnpoint.template.Position.x > 190 && spawnpoint.template.Position.x < 195 && spawnpoint.template.Position.z > -230 && spawnpoint.template.Position.z < -225 && spawnpoint.template.Position.y > -6 && spawnpoint.template.Position.y < -5) {
+                return true;
+            }
+        }
+
+        if (locationId === "tarkovstreets") {
+            //Abandoned Factory Marked Room
+            if (spawnpoint.template.Position.x > -133 && spawnpoint.template.Position.x < -129 && spawnpoint.template.Position.z > 265 && spawnpoint.template.Position.z < 275 && spawnpoint.template.Position.y > 8.5 && spawnpoint.template.Position.y < 11) {
+                return true;
+            }
+            //Chek 13 Marked Room
+            else if (spawnpoint.template.Position.x > 186 && spawnpoint.template.Position.x < 191 && spawnpoint.template.Position.z > 224 && spawnpoint.template.Position.z < 229 && spawnpoint.template.Position.y > -0.5 && spawnpoint.template.Position.y < 1.5) {
+                return true;
+            }
+        }
+
+        if (locationId === "lighthouse") {
+            //Lightkeeper marked room
+            if (spawnpoint.template.Position.x > 319 && spawnpoint.template.Position.x < 330 && spawnpoint.template.Position.z > 482 && spawnpoint.template.Position.z < 489 && spawnpoint.template.Position.y > 5 && spawnpoint.template.Position.y < 6.5) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private async markedAddExtraItemsAsync(spawnpoint: ISpawnpoint): Promise<void> {
