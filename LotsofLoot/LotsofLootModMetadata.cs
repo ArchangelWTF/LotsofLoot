625cc1a3b5ca7646d9bc30ada7062f84397d6981
Archangel <jesse@archangel.wtf>
2025-10-10 12:00:10 +0200

    Update refs

--- LotsofLoot/LotsofLootModMetadata.cs
+++ LotsofLoot/LotsofLootModMetadata.cs
@@ -1,5 +1,6 @@
 ﻿using SPTarkov.Server.Core.Models.Spt.Mod;
 using Version = SemanticVersioning.Version;
+using Range = SemanticVersioning.Range;
 
 namespace LotsofLoot
 {
@@ -10,10 +11,9 @@ namespace LotsofLoot
         public override string Author { get; init; } = "ArchangelWTF";
         public override List<string>? Contributors { get; init; } = ["RainbowPC"];
         public override Version Version { get; init; } = new("4.0.0");
-        public override Version SptVersion { get; init; } = new("4.0.0");
-
+        public override Range SptVersion { get; init; } = new("~4.0");
         public override List<string>? Incompatibilities { get; init; } = [];
-        public override Dictionary<string, Version>? ModDependencies { get; init; } = [];
+        public override Dictionary<string, Range>? ModDependencies { get; init; } = [];
         public override string? Url { get; init; } = "https://github.com/ArchangelWTF/LotsofLoot";
         public override bool? IsBundleMod { get; init; } = false;
         public override string License { get; init; } = "MIT";
